99bd3dbdacc5848b594058faeda2894c5305515e
Jay <jay@Jays-Air.lan>
2026-07-31 22:30:25 +0530

    Update default PORT to 3000 and refine server startup log

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ dotenv.config();
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 3080;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
